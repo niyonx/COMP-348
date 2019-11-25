ed1f37a0b88294d9d34a8fe86b2e25cb78b51f0e
esachoi <eunsuk_c@hotmail.com>
2019-11-25 00:36:30 -0500

    defined max str len

--- A4/q3_t1.c
+++ A4/q3_t1.c
@@ -1,25 +1,27 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#define MAX_STR_LEN 256
 
 struct Node {
-	char str[256];  //for the very last index "\0"
+	char str[MAX_STR_LEN];  //for the very last index "\0"
 	struct Node* next;
 };
 
-void insert_dictionary_order(char* c) 
+void insert_dictionary_order(char* s) 
 {
 
 }
 
 void print_list(struct Node* head) 
 {
-	struct Node* current_node = head;
-	while (current_node != NULL)
+	struct Node* temp = head;
+	while (temp != NULL)
 	{
-		printf("%s", current_node->str);
-		current_node = current_node->next;
+		printf("%s", temp->str);
+		temp = temp->next;
 	}
 }
+
 void push(struct Node** head_ref, char new_str) {
 	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
 	*new_node->str = new_str;
@@ -31,9 +33,9 @@ int main()
 {
 	struct Node* head = NULL;
 
-	char str[]= "This is a sample text. The file is terminated by a single dot: .";
+	char str[MAX_STR_LEN]= "This is a sample text. The file is terminated by a single dot: .";
 	printf("enter your string: ");
-	puts(str);
+	puts(str); //VS does not accept scanf, and online compilers don't accept strtok_s
 	//gets(str); 
 	char delimiter[] = " .,:";
 	char* p_token;
@@ -42,13 +44,13 @@ int main()
 	p_token = strtok_s(str, delimiter, &p_next_token);
 	for (int i = 0; p_token != NULL; i++)
 	{
-		printf("%s \n", p_token);
+		printf("%s  ", p_token);
 		p_token = strtok_s(NULL, delimiter, &p_next_token);	
 		push(&head, p_token);
 	}
 
 	print_list(head);
 
-	system("PAUSE");
+	system("\n PAUSE");
 	return 0;
 }
