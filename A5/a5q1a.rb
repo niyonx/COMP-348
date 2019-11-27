dbbb0956fb20d062453630b7adae52b64d8c1205
esachoi <eunsuk_c@hotmail.com>
2019-11-27 17:48:20 -0500

    Question1 partA

---
+++ A5/a5q1a.rb
@@ -0,0 +1,14 @@
+lines = File.readlines("assignment5q1.txt")
+line_count = lines.size
+text = lines.join 
+total_characters_nospaces = text.gsub(/\s+/, '').length
+word_count = text.split.length
+sentence_count = text.split(/\.|\?|!|;/).length 
+paragraph_count = text.split(/\n\n/).length 
+page_count = (sentence_count.to_f/50).ceil()
+
+puts "#{total_characters_nospaces} characters excluding spaces"
+puts "#{word_count} words"
+puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs" 
+puts "#{paragraph_count} page(s)" 
