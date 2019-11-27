cc3c925a24d4711c911ebe4784c63fbc8e693b87
Nigel Yong <23243585+niyonx@users.noreply.github.com>
2019-11-26 20:29:15 -0500

    Works, maybe do more test?
    >removeComments.exe program1.c
    
    #include <stdio.h>
    
    void sort_numbers_ascending(int number[], int count)
    {
            int temp, i, j, k;
            for (j = 0; j < count; ++j)
            {
                    for (k = j + 1; k < count; ++k)
                    {
                            if (number[j] > number[k])
                            {
                                    temp = number[j];
                                    number[j] = number[k];
                                    number[k] = temp;
                            }
                    }
            }
            printf("Numbers in ascending order:\n");
            for (i = 0; i < count; ++i)
                    printf("%d\n", number[i]);
    }
    
    >removeComments.exe program2.txt
    
    */
    void main()
    {
            static char s = "/*This is not a comment!*/";
    }
    
    >removeComments.exe program2.txt 1
    
    Nested format on.
    
    void main()
    {
            static char s = "/*This is not a comment!*/";
    }

--- A4/removeComments.c
+++ A4/removeComments.c
@@ -6,13 +6,92 @@ int main(int argc, char* argv[]) {
 		int arg = atoi(argv[2]);
 		if (arg == 1)
 		{
-			printf("Nested format on.\n");
+			printf("\nNested format on.\n");
+			printf("");
+			char ch;
+			bool single = false;
+			bool quote = false;
+			int multCount = 0;
+			FILE* fptr;
+			fptr = fopen(argv[1], "r");
+
+			if (fptr == NULL)
+			{
+				printf("Error!");
+				exit(1);
+			}
+			else
+			{
+				// check if file does not exist
+				if (fptr == NULL)
+				{
+					printf("Cannot open file \n");
+					exit(0);
+				}
+
+				// Read contents from file 
+				ch = fgetc(fptr);
+				while (ch != EOF)
+				{
+					if (single == false && multCount == 0) {
+						if (ch == '"') {
+							quote = !quote;
+						}
+						else if (single == false && multCount == 0 && quote == true) {
+							printf("%c", ch);
+							ch = fgetc(fptr);
+							continue;
+						}
+					}
+					if (ch == '\n') {
+						//printf("%c", 'n');
+						single = false;
+					}
+					else if (ch == '/') {
+						ch = fgetc(fptr);
+						if (ch == '*') {
+							multCount++;
+							//printf("%d",multCount);
+						}
+						else if (ch == '/')
+						{
+							//printf("%c", ch);
+							single = true;
+						}
+					}
+					else if (ch == '*') {
+						ch = fgetc(fptr);
+						if (ch == '"') {
+							printf("%c", ch);
+							quote = !quote;
+						}
+						if (ch == '/') {
+							multCount--;
+							//printf("%d", multCount);
+							ch = fgetc(fptr);
+						}
+					}
+
+
+
+					/*printf("%c", ch);*/
+					//printf("%d", single);
+					if (multCount == 0 && single == false || quote == true) {
+						printf("%c", ch);
+					}
+					ch = fgetc(fptr);
+				}
+
+				fclose(fptr);
+			}
 		}
-	}else if (argc == 2) {
+	}
+	else if (argc == 2) {
 		//printf("The argument supplied is %s\n", argv[1]);
 		printf("");
 		char ch;
 		bool single = false;
+		bool quote = false;
 		bool mult = false;
 		FILE* fptr;
 		fptr = fopen(argv[1], "r");
@@ -35,40 +114,66 @@ int main(int argc, char* argv[]) {
 			ch = fgetc(fptr);
 			while (ch != EOF)
 			{
+				if (single == false && mult == false) {
+					if (ch == '"') {
+						quote = !quote;
+					}
+					else if (single == false && mult == false && quote == true) {
+						printf("%c", ch);
+						ch = fgetc(fptr);
+						continue;
+					}
+				}
 				if (ch == '/') {
 					ch = fgetc(fptr);
-					if (ch == '*') {
+					if (ch == '"') {
+						printf("%c", ch);
+						quote = !quote;
+					}
+					else if (ch == '*') {
 						mult = true;
 					}
-					else
+					else if (ch == '/')
 					{
 						single = true;
 					}
 				}
-				else if (ch == '/n') {
+				else if (ch == '\n') {
 					single = false;
 				}
 				else if (ch == '*') {
 					ch = fgetc(fptr);
-					if (ch == '/') {
+					if (ch == '"') {
+						printf("%c", ch);
+						quote = !quote;
+					}
+					else if (ch == '/') {
+						if (mult == false)
+						{
+							printf("%c",'*');
+							printf("%c", ch);
+						}
 						mult = false;
 						ch = fgetc(fptr);
 					}
 				}
-
-				if (single == false && mult == false) {
+				else if (single == false && mult == false && quote == true) {
+					printf("%c", ch);
+					ch = fgetc(fptr);
+					continue;
+				}
+				if (single == false && mult == false || quote == true) {
 					printf("%c", ch);
 				}
 				ch = fgetc(fptr);
 			}
 
 			fclose(fptr);
-			return 0;
 		}
 	}
 	else {
 		printf("One argument expected.\n");
 	}
-
 	return 0;
+
 }
