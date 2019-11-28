b22aaddcb2df6e1330f403b8562036794ecf4dae
esachoi <eunsuk_c@hotmail.com>
2019-11-27 19:34:19 -0500

    Revert "fixed page number count"
    This reverts commit 859116316e858f2be2033a920cad318907d2295a.

--- A5/a5q1a.rb
+++ A5/a5q1a.rb
@@ -5,7 +5,7 @@ total_characters_nospaces = text.gsub(/\s+/, '').length
 word_count = text.split.length
 sentence_count = text.split(/\.|\?|!|;/).length 
 paragraph_count = text.split(/\n\n/).length 
-page_count = IO.readlines("assignment5q1.txt").size
+page_count = (sentence_count.to_f/50).ceil()
 
 puts "#{total_characters_nospaces} characters excluding spaces"
 puts "#{word_count} words"
