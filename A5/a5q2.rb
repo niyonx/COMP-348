6daceab73c1f2755cc9614147eb270050ce80abc
esachoi <eunsuk_c@hotmail.com>
2019-11-30 13:48:26 -0500

    minor changes in switch cases, implement inventory class

--- A5/a5q2.rb
+++ A5/a5q2.rb
@@ -1,6 +1,5 @@
-puts "Select one of the following options: 1. show the inventory, 2. import
-a listing, 3. export to listing, 4. exit. "
-input = gets.chomp.to_i
+puts "Select one of the following options: \n 1. show the inventory \n 2. import a listing \n 3. export to listing \n 4. exit. "
+input = gets.to_i
 case input
 when 1
     puts "you have selected inventory display option"
@@ -10,6 +9,50 @@ when 3
     puts "you have selected export to list option"
 when 4
     puts "you have selected to exit the program"
+    abort ("exiting the program...")
 else 
     puts "invalid input detected"
+end
+
+def showInventory()
+end
+
+def importlisting()
+end
+
+def exportlisting()
+end
+
+def addtoInventory()
+end
+
+class Inventory
+    @@total=0
+    #constructor
+    def initialize(category, batteryLife, modelNum, color, manufacturer, status, yearBuilt, price, features)
+        @@total +=1
+        @category = category
+        @batteryLife = batteryLife
+        @modelNum = modelNum
+        @color = color
+        @manufacturer = manufacturer
+        @status = status
+        @yearBuilt = yearBuilt 
+        @price =price
+        @features = features
+    end 
+    
+    #accessor for each attribute
+    def printItem
+        puts @category
+        puts @batteryLife
+        puts @modelNum
+        puts @color
+        puts @manufacturer
+        puts @status
+        puts @yearBuilt
+        puts @price
+        puts @features
+    end 
+
 end
